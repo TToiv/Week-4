781ee7bacacdad1fdff2c4c6feb601c27060657b
TToiv <toit1_15@uni.worc.ac.uk>
2017-10-16 11:25:38 +0100

    Fixed alert

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var key = “pen”;
-var value = “blue”;
+var key = "pen";
+var value = "blue";
 
 window.localStorage.setItem( key, value );
 
 alert("key + is + value");
 
-var key = “pen”;
+var key = "pen";
 var value = window.localStorage.getItem(key);
 
 alert("key + is + value");
