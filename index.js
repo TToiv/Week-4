328280afaa88d2d051a2901cb7675a3d025fff2d
TToiv <toit1_15@uni.worc.ac.uk>
2017-10-16 11:08:59 +0100

    added alert

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ var value = “blue”;
 
 window.localStorage.setItem( key, value );
 
+alert("key + is + value");
+
 var key = “pen”;
 var value = window.localStorage.getItem(key);
+
+alert("key + is + value");
