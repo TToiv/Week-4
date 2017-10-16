c434554c6b44af2f7d661708ed279b820cf658bd
TToiv <toit1_15@uni.worc.ac.uk>
2017-10-16 11:32:24 +0100

    Fixed string

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var key = "pen";
 var value = "blue";
 
-window.localStorage.setItem( key, value );
+window.localStorage.setItem(key, value);
 
-alert("key + is + value");
+alert(key + " is " + value);
 
 var key = "pen";
 var value = window.localStorage.getItem(key);
 
-alert("key + is + value");
+alert(key + " is " + value);
